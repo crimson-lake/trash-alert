9204430b0cadf6a4cf274d482edb75f9b83aeb6f
crimson-lake <sandra.zielinska@protonmail.com>
2019-12-27 19:29:49 +0100

    Initial commit

---
+++ src/main/java/pl/zielinska/trashAlert/TrashAlertApplication.java
@@ -0,0 +1,13 @@
+package pl.zielinska.trashAlert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrashAlertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrashAlertApplication.class, args);
+	}
+
+}
