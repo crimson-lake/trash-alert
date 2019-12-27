5948779f1213dc3da8b35b8829e8e26b87985880
crimson-lake <sandra.zielinska@protonmail.com>
2019-12-27 19:29:49 +0100

    Initial commit

---
+++ src/test/java/pl/zielinska/trashAlert/TrashAlertApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.zielinska.trashAlert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrashAlertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
