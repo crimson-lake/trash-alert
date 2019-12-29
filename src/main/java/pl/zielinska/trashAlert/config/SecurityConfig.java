56f74cee690121929c1d006b33b80284020bda24
crimson-lake <sandra.zielinska@protonmail.com>
2019-12-29 12:39:00 +0100

    configured basic spring security: TODO - extend it in future

---
+++ src/main/java/pl/zielinska/trashAlert/config/SecurityConfig.java
@@ -0,0 +1,31 @@
+package pl.zielinska.trashAlert.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        User.UserBuilder users = User.withDefaultPasswordEncoder();
+
+        auth.inMemoryAuthentication()
+                .withUser(users.username("user").password("123456").roles("USER"));
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/users**").permitAll();
+
+    }
+
+}
