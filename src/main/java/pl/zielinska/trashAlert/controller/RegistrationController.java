90c8ab34bfd2a31f39fbe55a1b394527e1e26b79
crimson-lake <sandra.zielinska@protonmail.com>
2020-03-22 01:11:03 +0100

    added input sanitization for all controllers

--- src/main/java/pl/zielinska/trashAlert/controller/RegistrationController.java
+++ src/main/java/pl/zielinska/trashAlert/controller/RegistrationController.java
@@ -2,13 +2,10 @@ package pl.zielinska.trashAlert.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import pl.zielinska.trashAlert.DTO.UserDto;
@@ -22,11 +19,6 @@ public class RegistrationController {
 
     @Autowired
     private UserService userService;
-
-    @InitBinder
-    public void initBinder(WebDataBinder binder) {
-        binder.registerCustomEditor(String.class, new StringTrimmerEditor(true));
-    }
 
     @GetMapping("/register")
     public String showRegistrationForm(Model model) {
