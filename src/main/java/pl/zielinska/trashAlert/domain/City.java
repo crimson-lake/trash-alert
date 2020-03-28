f50b0c2e55c15aa81d52152a18526b9958217400
crimson-lake <sandra.zielinska@protonmail.com>
2020-03-28 09:50:33 +0100

    FIX: translation + warsaw coordinates

--- src/main/java/pl/zielinska/trashAlert/domain/City.java
+++ src/main/java/pl/zielinska/trashAlert/domain/City.java
@@ -7,7 +7,7 @@ public enum City {
     ALL(19.08, 51.55, 6, "city.all"),
     KRAKOW(19.93,50.06, 12, "city.cracow"),
     POZNAN(16.93, 52.41, 12, "city.poznan"),
-    WARSAW(21.00, 52.13, 12, "city.warsaw"),
+    WARSAW(21.00, 52.23, 12, "city.warsaw"),
     WROCLAW(17.03, 51.10, 12, "city.wroclaw"),
     TRICITY(18.59,54.44, 11, "city.3city");
 
