04eac515ef708f7cc3fd57e493c8cf1b58591f06
crimson-lake <sandra.zielinska@protonmail.com>
2020-04-04 18:39:03 +0200

    error controller and custom error page

---
+++ src/main/java/pl/zielinska/trashAlert/controller/CustomErrorController.java
@@ -0,0 +1,19 @@
+package pl.zielinska.trashAlert.controller;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class CustomErrorController implements ErrorController {
+
+    @GetMapping("/error")
+    public String showErrorPage() {
+        return "error";
+    }
+
+    @Override
+    public String getErrorPath() {
+        return "/error";
+    }
+}
