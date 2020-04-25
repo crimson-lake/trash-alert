933686f73efabe916c5341987ae762e9ae6469f0
crimson-lake <sandra.zielinska@protonmail.com>
2020-04-25 18:04:35 +0200

    changes in stucture

--- src/main/java/pl/zielinska/trashAlert/dao/CommentRepository.java
+++
@@ -1,7 +0,0 @@
-package pl.zielinska.trashAlert.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pl.zielinska.trashAlert.domain.Comment;
-
-public interface CommentRepository extends JpaRepository<Comment, Integer> {
-}
--- src/main/java/pl/zielinska/trashAlert/dao/TagRepository.java
+++
@@ -1,9 +0,0 @@
-package pl.zielinska.trashAlert.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pl.zielinska.trashAlert.domain.Tag;
-
-public interface TagRepository extends JpaRepository<Tag, Integer> {
-
-    Tag findByTag(String tag);
-}
