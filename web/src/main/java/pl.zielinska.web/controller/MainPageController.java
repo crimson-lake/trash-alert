a89c158f5d970b6b0b5674c3c79cd3189be232c8
crimson-lake <sandra.zielinska@protonmail.com>
2020-04-26 22:45:29 +0200

    navbar links - show username or link to login form if no active user

--- web/src/main/java/pl.zielinska.web/controller/MainPageController.java
+++ web/src/main/java/pl.zielinska.web/controller/MainPageController.java
@@ -1,13 +1,28 @@
 package pl.zielinska.web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import pl.zielinska.outdoor.domain.User;
+import pl.zielinska.outdoor.service.UserService;
+
+import javax.servlet.http.HttpServletRequest;
+import java.security.Principal;
 
 @Controller
 public class MainPageController {
 
+    @Autowired
+    private UserService userService;
+
     @GetMapping("/")
-    public String mainPage() {
+    public String mainPage(Model model, HttpServletRequest request) {
+        final Principal principal = request.getUserPrincipal();
+        if (principal != null) {
+            final User activeUser = userService.findByUsername(principal.getName());
+            model.addAttribute("username", activeUser.getUsername());
+        }
         return "main-page";
     }
 }
