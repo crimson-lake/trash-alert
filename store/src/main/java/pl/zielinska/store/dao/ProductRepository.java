8cdb138b13d6ade435ecb01bbe6b84746069dc9a
crimson-lake <sandra.zielinska@protonmail.com>
2020-05-05 23:37:22 +0200

    move repositories to domain module

--- store/src/main/java/pl/zielinska/store/dao/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package pl.zielinska.store.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pl.zielinska.store.domain.Product;
-
-public interface ProductRepository {
-}
