eda2e3d64973101751f774bfe112d5b42a92b5a1
crimson-lake <sandra.zielinska@protonmail.com>
2020-06-09 21:28:51 +0200

    Sorting - basic implementation

--- outdoor-search/src/main/java/pl/zielinska/outdoor/service/AdServiceImpl.java
+++ outdoor-search/src/main/java/pl/zielinska/outdoor/service/AdServiceImpl.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import pl.zielinska.model.repository.AdRepository;
 import pl.zielinska.model.domain.Ad;
@@ -34,6 +35,11 @@ public class AdServiceImpl implements AdService{
     @Override
     public List<AdDto> findAllDto() {
         return adConverter.createFromEntities(adRepository.findAll());
+    }
+
+    @Override
+    public List<AdDto> findAllDto(Sort sort) {
+        return adConverter.createFromEntities(adRepository.findAll(sort));
     }
 
     @Override
